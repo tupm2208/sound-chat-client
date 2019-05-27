f16f84ac4b509fd1df102749ea70d51964ce4634
tupm <https://tupm@bitbucket.kaopiz.com/scm/ecb/ec-web.git>
2019-05-27 10:46:24 +0700

    change pusher authen

--- src/core/configs/base.js
+++ src/core/configs/base.js
@@ -3,7 +3,7 @@ export class BaseConfig {
     YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3';
     YOUTUBE_SEARCH_URL = `https://www.googleapis.com/youtube/v3/search`;
     BASE_URL = `http://localhost:8000/api/`;
-    PUSHER_URL = `http://localhost:8000/`;
+    PUSHER_URL = `http://localhost:8000/api/`;
     PUSHER_NAMESPACE = '';
     PUSER_KEY = '8ca50ca8ff937987bdce';
 }
